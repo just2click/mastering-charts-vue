d37da9be9d30f60a8d6215d4a49bc3b8d45baca7
Dror a <just2click@outlook.com>
2021-03-21 22:17:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello-world :data="data" :id="g1" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: { HelloWorld },
+  name: 'App',
+  data () {
+    return {
+      data: [
+        {
+          time: 0.5,
+          value: 75
+        },
+        {
+          time: 1,
+          value: 15
+        },
+        {
+          time: 1.5,
+          value: 90
+        },
+        {
+          time: 2,
+          value: 30
+        },
+        {
+          time: 2.5,
+          value: 10
+        },
+        {
+          time: 3,
+          value: 99
+        },
+        {
+          time: 3.5,
+          value: 10
+        },
+        {
+          time: 4,
+          value: 96
+        },
+        {
+          time: 4.5,
+          value: 79
+        },
+        {
+          time: 5,
+          value: 10
+        },
+        {
+          time: 6,
+          value: 90
+        },
+        {
+          time: 6.5,
+          value: 10
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="hello">
+    <v-chart v-bind:chartData="setLinearGraphData"></v-chart>
+    <v-chart v-bind:chartData="setLinearGraphData2"></v-chart>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['id', 'data'],
+  data () {
+    return {
+      chartData: {
+        chartType: 'lineGraph',
+        selector: 'chart',
+        title: 'Student Grades',
+        subtitle: '2018 - 2019',
+        metric: ['value'],
+        dim: 'time',
+        label: true,
+        width: 600,
+        height: 300,
+        data: []
+      },
+      chartData2: {
+        chartType: 'lineGraph',
+        selector: 'chart2',
+        title: 'Test Values',
+        subtitle: '2018 - 2019',
+        metric: ['value'],
+        dim: 'time',
+        label: true,
+        width: 600,
+        height: 300,
+        data: []
+      }
+    }
+  },
+  computed: {
+    setLinearGraphData () {
+      this.setData()
+      return this.chartData
+    },
+    setLinearGraphData2 () {
+      this.setData2()
+      return this.chartData2
+    }
+  },
+  methods: {
+    setData () {
+      this.chartData.data = this.data
+    },
+    setData2 () {
+      this.chartData2.data = this.data
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
